b2c3575a6888aa59cfdfa8a18ccd66a1c3595fc6
bel1c10ud <mail@bellclou.de>
2022-04-06 18:20:55 +0900

    add authorization page, connect api

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,10 +4,14 @@ import style from './Button.module.css';
 
 export default function Button(props: {
   children?: string,
-  onClick?: MouseEventHandler<HTMLButtonElement>
+  onClick?: MouseEventHandler<HTMLButtonElement>,
+  disabled?: boolean
 }) {
   return (
-    <button className={style.self} onClick={props.onClick}>
+    <button className={style.self} 
+    onClick={props.onClick}
+    disabled={props.disabled}
+    >
       {props.children}
     </button>
   )
---
+++ pages/authorization.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+import AuthorizationLayout from '../components/template/AuthorizationLayout';
+
+const AuthorizationPage: NextPage = (props) => {
+  return (
+    <AuthorizationLayout />
+  )
+}
+
+export default AuthorizationPage
