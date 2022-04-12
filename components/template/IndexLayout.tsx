58f72bce96102001e903804e3fad8a4522a483e2
bel1c10ud <mail@bellclou.de>
2022-04-12 16:38:58 +0900

    add about link

--- components/template/IndexLayout.tsx
+++ components/template/IndexLayout.tsx
@@ -8,6 +8,7 @@ import style from './IndexLayout.module.css';
 import Intro from '../Intro';
 import Button from '../Button';
 import Store from '../Store';
+import Link from 'next/link';
 
 export default function IndexLayout() {
   const router = useRouter();
@@ -18,6 +19,7 @@ export default function IndexLayout() {
     return (
       <div className={style.self}>
         <Button onClick={() => router.push('/api/clear')}>로그아웃</Button>
+        <li><Link href="/about">이 프로젝트의 동작 방식에 대하여</Link></li>
         <Store access_token={authObj.access_token} />
       </div>
     )
@@ -25,7 +27,8 @@ export default function IndexLayout() {
     return (
       <div className={style.self}>
         <Intro />
-        <Button onClick={() => router.push('/authorization')}>로그인</Button> 
+        <Button onClick={() => router.push('/authorization')}>로그인</Button>
+        <li><Link href="/about">이 프로젝트의 동작 방식에 대하여</Link></li>
       </div>
     )
   }
