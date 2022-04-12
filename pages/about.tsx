2378a371fcd8732d5f18eab57af393377c05cda5
bel1c10ud <mail@bellclou.de>
2022-04-12 16:31:24 +0900

    add about page

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+import AboutLayout from '../components/template/AboutLayout'
+
+const AboutPage: NextPage = (props) => {
+  return (
+    <AboutLayout />
+  )
+}
+
+export default AboutPage
