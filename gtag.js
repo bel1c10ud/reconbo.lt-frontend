5ace1a9be317eb756fc36e13862750f374222480
bel1c10ud <mail@bellclou.de>
2022-05-17 18:20:19 +0900

    ga fix

--- gtag.js
+++ gtag.js
@@ -1,15 +1,19 @@
 export const GA_TRACKING_ID = "G-WZT2V39YP6";
 
 export const pageview = (url) => {
-  window.gtag("config", GA_TRACKING_ID, {
-    page_path: url,
-  });
+  if(window.gtag) {
+    window.gtag("config", GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
 };
 
 export const event = ({ action, category, label, value }) => {
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  });
+  if(window.gtag) {
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { NEXT_DATA } from 'next/dist/shared/lib/utils';
+import { Html, Head, Main, NextScript, DocumentProps } from 'next/document'
 import { GA_TRACKING_ID } from '../gtag'
 
-export default function Document() {
+export default function Document(props: DocumentProps) {
+
   return (
     <Html>
       <Head>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></script>
-        <script dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `
-        }} />
+{ props['__NEXT_DATA__']['buildId'] !== 'development' && (
+        <>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></script>
+          <script dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+  
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `
+          }} />
+        </>
+)}
       </Head>
       <body>
         <Main />
