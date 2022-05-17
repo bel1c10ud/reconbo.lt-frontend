88613043e95162c0cb14f741d50f33c79616612e
bel1c10ud <mail@bellclou.de>
2022-05-17 17:09:07 +0900

    add ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import App from 'next/app'
-import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { RecoilRoot} from 'recoil';
 
 import '../styles/globals.css'
@@ -9,12 +9,26 @@ import type { AppContext, AppProps } from 'next/app'
 import type { CookieType } from './../type';
 import InitManager from '../components/InitManager';
 import PopupLayer from '../components/PopupLayer';
+import { useEffect } from 'react';
+import * as gtag from './../gtag';
 
 interface CustomAppProps extends AppProps{ 
   cookies: CookieType[]
 }
 
 function MyApp({ Component, pageProps, cookies }: CustomAppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <RecoilRoot>
       <InitManager cookies={cookies} />
